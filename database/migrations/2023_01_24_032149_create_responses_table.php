fc090dfd5310d6837d5edee091ead15bbcd2868e
Ahmad Septian <ahmadseptian576@gmail.com>
2023-01-24 10:56:51 +0700

    first commit

---
+++ database/migrations/2023_01_24_032149_create_responses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResponsesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('responses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('users_id');
+            $table->foreignId('complaints_id');
+            $table->string('response');
+            $table->string('status_complaint');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('responses');
+    }
+}
